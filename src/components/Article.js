da4102cc8189ff166344a1b4f9eec0759f10f6b9
moses <moses.kingi@moringaschool.com>
2023-11-30 16:04:33 +0300

    emojis calculation fuctions bonus

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,10 +2,20 @@ import React from "react";
 
 
 function Article ({title, date, preview, minutes}) {
+   // Function to calculate emojis based on reading time
+   const calculateEmojis = (time) => {
+    if (time < 30) {
+      const coffeeEmojis = Math.ceil(time/ 5);
+      return "☕️".repeat(coffeeEmojis);
+    } else {
+      const bentoEmojis = Math.ceil(time/ 10);
+      return "🍱".repeat(bentoEmojis);
+    }
+  };
   return (
     <article>
 <h3>{title}</h3>
-<small>{date}</small><span>{minutes}min read</span>
+<small>{date}</small><span>{calculateEmojis(minutes)} {minutes} min read</span>
 <p>{preview} </p>
 </article>
   );
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,16 +2,35 @@ import React from "react";
 import blogData from "../data/blog";
 import Article from "./Article";
 
-function ArticleList () {
+function ArticleList() {
   const posts = blogData.posts;
+
+  // Function to calculate emojis based on reading time
+  const calculateEmojis = (time) => {
+    if (time < 30) {
+      const coffeeEmojis = Math.ceil(time / 5);
+      return "☕️".repeat(coffeeEmojis);
+    } else {
+      const bentoEmojis = Math.ceil(time / 10);
+      return "🍱".repeat(bentoEmojis);
+    }
+  };
 
   return (
     <main>
       {posts.map((post) => (
-        <Article key={post.id} title={post.title} preview={post.preview} date={post.date} />
+        <Article
+          key={post.id}
+          title={post.title}
+          preview={post.preview}
+          date={post.date}
+          minutes={post.minutes} 
+          emojis={calculateEmojis(post.minutes)}
+        />
       ))}
     </main>
   );
 }
 
 export default ArticleList;
+
